15ac0f8d748b323aedb6231bf56956c9b7b1faa7
smarthomelawyer <74792554+smarthomelawyer@users.noreply.github.com>
2022-06-05 18:34:13 -0400

    Update wireguard-install.sh

--- setup/wireguard-install.sh
+++ setup/wireguard-install.sh
@@ -79,7 +79,7 @@ msg_ok "Updated Container OS"
 msg_info "Installing Dependencies"
 apt-get install -y curl &>/dev/null
 apt-get install -y sudo &>/dev/null
-apt-get install -y gunicorn &>/dev/null
+
 msg_ok "Installed Dependencies"
 
 msg_info "Installing WireGuard (using pivpn.io)"
@@ -88,44 +88,7 @@ chmod +x install.sh
 ./install.sh --unattended options.conf &>/dev/null
 msg_ok "Installed WireGuard"
 
-msg_info "Installing pip3"
-apt-get install python3-pip -y &>/dev/null
-pip install flask &>/dev/null
-pip install ifcfg &>/dev/null
-pip install flask_qrcode &>/dev/null
-pip install icmplib &>/dev/null
-msg_ok "Installed pip3"
 
-msg_info "Installing WGDashboard"
-WGDREL=$(curl -s https://api.github.com/repos/donaldzou/WGDashboard/releases/latest \
-| grep "tag_name" \
-| awk '{print substr($2, 2, length($2)-3) }') \
-
-git clone -b ${WGDREL} https://github.com/donaldzou/WGDashboard.git /etc/wgdashboard &>/dev/null
-cd /etc/wgdashboard/src
-sudo chmod u+x wgd.sh
-sudo ./wgd.sh install &>/dev/null
-sudo chmod -R 755 /etc/wireguard
-msg_ok "Installed WGDashboard"
-
-msg_info "Creating Service"
-service_path="/etc/systemd/system/wg-dashboard.service"
-echo "[Unit]
-After=netword.service
-
-[Service]
-WorkingDirectory=/etc/wgdashboard/src
-ExecStart=/usr/bin/python3 /etc/wgdashboard/src/dashboard.py
-Restart=always
-
-
-[Install]
-WantedBy=default.target" > $service_path
-sudo chmod 664 /etc/systemd/system/wg-dashboard.service
-sudo systemctl daemon-reload
-sudo systemctl enable wg-dashboard.service &>/dev/null
-sudo systemctl start wg-dashboard.service
-msg_ok "Created Service"
 
 PASS=$(grep -w "root" /etc/shadow | cut -b6);
   if [[ $PASS != $ ]]; then
